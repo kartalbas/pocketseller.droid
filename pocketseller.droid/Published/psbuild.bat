82a53e5de59155f7ef82a8f6e8fd34f69a82f06a
Mehmet Kartalbas <kartalbas@gmail.com>
2021-10-02 22:53:54 +0200

    adapt version for playstore upload

--- pocketseller.droid/Published/psbuild.bat
+++ pocketseller.droid/Published/psbuild.bat
@@ -1,7 +1,7 @@
-msbuild.exe ..\..\orderline.sln /p:Configuration=Release /t:Clean
-msbuild.exe ..\..\orderline.droid\orderline.droid.csproj /p:Configuration=Release /t:PackageForAndroid
-"d:\bin\Android\sdk\build-tools\30.0.3\zipalign.exe" -p 4 ..\..\orderline.droid\bin\Release\orderline.droid.apk ..\..\orderline.droid\bin\Release\orderline.droid-signed.apk
-java.exe -jar "d:\bin\android\sdk\build-tools\30.0.3\lib\apksigner.jar" sign --ks ..\..\orderline.droid\Published\simetrix.keystore --ks-pass pass:Tikmonca13 --ks-key-alias simetrix --key-pass pass:Tikmonca13 --min-sdk-version 23 --max-sdk-version 30  ..\..\orderline.droid\bin\Release\orderline.droid-signed.apk
-copy ..\..\orderline.droid\bin\Release\orderline.droid-signed.apk ..\..\orderline.droid\Published\orderline.droid.000.apk
+msbuild.exe ..\..\pocketseller.sln /p:Configuration=Release /t:Clean
+msbuild.exe ..\..\pocketseller.droid\pocketseller.droid.csproj /p:Configuration=Release /t:PackageForAndroid
+"d:\bin\Android\sdk\build-tools\30.0.3\zipalign.exe" -p 4 ..\..\pocketseller.droid\bin\Release\pocketseller.droid.apk ..\..\pocketseller.droid\bin\Release\pocketseller.droid-signed.apk
+java.exe -jar "d:\bin\android\sdk\build-tools\30.0.3\lib\apksigner.jar" sign --ks ..\..\pocketseller.droid\Published\simetrix.keystore --ks-pass pass:Tikmonca13 --ks-key-alias simetrix --key-pass pass:Tikmonca13 --min-sdk-version 23 --max-sdk-version 30  ..\..\pocketseller.droid\bin\Release\pocketseller.droid-signed.apk
+copy ..\..\pocketseller.droid\bin\Release\pocketseller.droid-signed.apk ..\..\pocketseller.droid\Published\pocketseller.droid.000.apk
 
 
